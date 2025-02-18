3bd8ff74784dff533e7707e2fae32bc248ebf1c8
Raji G <rajith0417@gmail.com>
2025-02-18 22:52:35 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { TranscriptionComponent } from './components/transcription/transcription.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, MainContentComponent, TranscriptionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/transcription/transcription.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transcription',
+  standalone: true,
+  imports: [],
+  templateUrl: './transcription.component.html',
+  styleUrl: './transcription.component.css'
+})
+export class TranscriptionComponent {
+
+}
