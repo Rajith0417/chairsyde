6cceb869b1920d4c34b470f7b085338c16237cc8
Raji G <rajith0417@gmail.com>
2025-02-20 01:00:48 +0530

    css: css modifications done to all components except transcription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { TranscriptionComponent } from './components/transcription/transcription.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, MainContentComponent, TranscriptionComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent, MainContentComponent, TranscriptionComponent, FooterComponent, HeaderComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
